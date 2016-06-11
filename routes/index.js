8b28029aeb537a69798215a2f34b1f7448ae8e80
Mark <mark@remarkablemark.org>
2016-06-11 01:45:09 -0400

    Tidy all files
    - Use 4 spaces instead of 2
    - Add comments
    - Enable strict mode
    - Update `package.json` with description, author, and license
    - Use exact version of npm packages
    - Remove users route
    - Add swap files to gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * GET index page.
+ */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
+/**
+ * Export router.
+ */
 module.exports = router;
