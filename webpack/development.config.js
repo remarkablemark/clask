3c1a0736dea7900335efeb5800ac3e492909e063
Mark <mark@remarkablemark.org>
2016-12-08 20:10:38 -0500

    Create webpack development config
    https://github.com/remarkablemark/react-engine-template/blob/master/webpack/development.config.js

---
+++ webpack/development.config.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var path = require('path');
+var webpack = require('webpack');
+
+var PORT = 3001;
+var HOST = process.env.IP || 'localhost';
+var URL = 'http://' + HOST + ':' + PORT;
+
+/**
+ * Development webpack.
+ */
+module.exports = {
+    entry: [
+        path.join(__dirname, '../src/main.js'),
+        'webpack/hot/only-dev-server',
+        'webpack-dev-server/client?' + URL
+    ],
+
+    output: {
+        path: path.join(__dirname, '../build/'),
+        filename: 'bundle.js',
+        publicPath: URL + '/build'
+    },
+
+    debug: true,
+    devtool: 'eval-source-map',
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015', 'react-hmre']
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+
+    devServer: {
+        quiet: true,
+        inline: true,
+        hot: true,
+        host: HOST,
+        port: PORT
+    }
+};
