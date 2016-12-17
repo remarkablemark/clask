a2db50bef1730ed9b58f0fcf0a23d83cf653c15d
Mark <mark@remarkablemark.org>
2016-12-17 18:09:54 -0500

    Style the chat <Form> with material-ui

--- src/chat/Form.js
+++ src/chat/Form.js
@@ -4,6 +4,38 @@
  * Module dependencies.
  */
 import React from 'react';
+import Divider from 'material-ui/Divider';
+import IconButton from 'material-ui/IconButton';
+import TextField from 'material-ui/TextField';
+
+// constants
+const buttonWidth = 48;
+
+// styles
+const styles = {
+    container: {
+        position: 'fixed',
+        right: 0,
+        bottom: 0,
+        left: 0,
+        margin: '0 auto',
+        width: '100%',
+        height: buttonWidth * 2
+    },
+    line: {
+        marginBottom: buttonWidth / 2
+    },
+    button: {
+        position: 'absolute',
+        left: 0,
+        width: buttonWidth
+    },
+    input: {
+        position: 'absolute',
+        right: 0,
+        left: buttonWidth
+    }
+};
 
 /**
  * Form component.
@@ -36,9 +68,24 @@ export default class Form extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this._handleSubmit}>
-                <input type='text' value={this.state.value} onChange={this._handleChange} />
-                <input type='submit' value='Enter' />
+            <form onSubmit={this._handleSubmit} style={styles.container}>
+                <Divider style={styles.line} />
+                <div style={styles.button}>
+                    <IconButton iconClassName='material-icons' type='submit'>
+                        add
+                    </IconButton>
+                </div>
+                <div style={styles.input}>
+                    <TextField
+                        autoComplete='off'
+                        autoCorrect='off'
+                        fullWidth={true}
+                        hintText='Message'
+                        onChange={this._handleChange}
+                        spellCheck='true'
+                        value={this.state.value}
+                    />
+                </div>
             </form>
         );
     }
