20fba7090e80a53199f18a409fce69b238de6e4e
Mark <mark@remarkablemark.org>
2016-12-17 21:33:34 -0500

    Remove the placeholder text and styles in app <Index>
    Only render <Chat>

--- src/app/Index.js
+++ src/app/Index.js
@@ -6,21 +6,11 @@
 import React from 'react';
 import Chat from '../chat/Chat';
 
-const style = {
-    padding: 50,
-    fontSize: 14,
-    fontFamily: '"Lucida Grande", Helvetica, Arial, sans-serif'
-};
-
 /**
  * Index component.
  */
 export default function Index() {
     return (
-        <div style={style}>
-            <h1>Express-Template</h1>
-            <p>Welcome to Express-Template</p>
-            <Chat />
-        </div>
+        <Chat />
     );
 }
