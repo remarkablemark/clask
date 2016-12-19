4cf423ac063a6f4d59288d115c6392c06c27cc7b
Mark <mark@remarkablemark.org>
2016-12-18 20:42:45 -0500

    Load all messages before rendering <Chat>
    This ensures the user gets the chat messages before being able
    to send messages.
    
    Get the messages json from `/api/messages` using superagent.

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -10,13 +10,39 @@ import Form from './Form';
 /**
  * Chat component.
  */
-export default function Chat(props) {
-    return (
-        <div>
-            <MessageList messages={props.messages} />
-            <Form />
-        </div>
-    );
+export default class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            messages: props.messages
+        };
+    }
+
+    componentDidMount() {
+        window.requirejs(['superagent'], (request) => {
+            request.get('/api/messages', (error, response) => {
+                if (error || !response.ok) {
+                    return console.log(error, response); // eslint-disable-line no-console
+                }
+                this.setState({
+                    messages: response.body,
+                    isLoaded: true
+                });
+            });
+        });
+    }
+
+    render() {
+        const { isLoaded, messages } = this.state;
+        if (!isLoaded) return null;
+        return (
+            <div>
+                <MessageList messages={messages} />
+                <Form />
+            </div>
+        );
+    }
 }
 
 Chat.propTypes = {
