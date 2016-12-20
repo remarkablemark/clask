653e0aee329dce02b5961c66b4e987e5276b13bb
Mark <mark@remarkablemark.org>
2016-12-19 23:58:11 -0500

    Modularize <SignUp> by creating wrapper <Form>
    <Form> will be the layout for the sign-up and sign-in components.
    The props are as follows:
    - onSubmit
    - heading
    - children

---
+++ src/user/Form.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+import React from 'react';
+import Paper from 'material-ui/Paper';
+import Center from './Center';
+
+// styles
+import { grey700 } from 'material-ui/styles/colors';
+import { gutter } from '../chat/styles';
+const styles = {
+    heading: {
+        color: grey700
+    },
+    container: {
+        margin: '0 auto',
+        maxWidth: 400
+    },
+    content: {
+        margin: `0 ${gutter / 2}px`
+    }
+};
+
+/**
+ * Form component.
+ */
+export default function Form(props) {
+    return (
+        <Center>
+            <form style={styles.container} onSubmit={props.onSubmit}>
+                {/* heading */}
+                {
+                    props.heading &&
+                    <h1 style={styles.heading}>
+                        {props.heading}
+                    </h1>
+                }
+                <Paper style={styles.content}>
+                    {props.children}
+                </Paper>
+            </form>
+        </Center>
+    );
+}
+
+Form.propTypes = {
+    onSubmit: React.PropTypes.func,
+    heading: React.PropTypes.string,
+    children: React.PropTypes.node
+};
