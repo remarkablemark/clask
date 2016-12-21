6dbadbe47f39186407e2fca8f29ef1470e0531a4
Mark <mark@remarkablemark.org>
2016-12-21 17:59:12 -0500

    Create route `/api/auth` that authenticates user via POST request
    Checks user email and password and returns a success or error
    response with message.

--- routes/api/index.js
+++ routes/api/index.js
@@ -26,6 +26,55 @@ router.get('/messages', (req, res, next) => {
 });
 
 /**
+ * POST: /api/auth
+ */
+router.post('/auth', (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email) return res.json({});
+    if (!password) return res.json({});
+
+    User.findOne({ email }, (error, user) => {
+        if (error) {
+            debug('error find user', error)
+            return res.status(500).json({});
+        }
+
+        // user not found
+        if (!user) {
+            return res.json({
+                success: false,
+                message: 'Sorry, you entered an incorrect email address or password.'
+            });
+
+        // user found
+        } else {
+            const { _id, name, username, email } = user;
+
+            // authentication success
+            if (password === user.password) {
+                res.json({
+                    success: true,
+                    message: 'Authentication successful.',
+                    user: {
+                        _id,
+                        name,
+                        username,
+                        email
+                    }
+                });
+
+            // authentication failure
+            } else {
+                res.json({
+                    success: false,
+                    message: 'Sorry, you entered an incorrect email address or password.'
+                });
+            }
+        }
+    });
+});
+
+/**
  * GET: /api/users
  */
 router.get('/users', (req, res, next) => {
