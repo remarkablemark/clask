c740aaf32812720d07fb09e4995177cbf278d5ba
Mark <mark@remarkablemark.org>
2016-12-20 22:07:15 -0500

    Allow route `/api/users` to accept POST request to create new user
    When saving new user, check for duplication of the fields `email`
    and `username`.

--- routes/api/index.js
+++ routes/api/index.js
@@ -49,6 +49,55 @@ router.get('/users', (req, res, next) => {
 });
 
 /**
+ * POST: /api/users
+ */
+router.post('/users', (req, res, next) => {
+    // try to save user
+    const user = new User(req.body);
+    user.save((err, user) => {
+        if (err) {
+            // duplicate key error
+            if (err.code === 11000) {
+
+                // duplicate email
+                if (/email/.test(err.errmsg)) {
+                    return res.json({
+                        success: false,
+                        error: {
+                            field: 'email',
+                            text: 'Email already exists.'
+                        }
+                    });
+
+                // duplicate username
+                } else if (/username/.test(err.errmsg)) {
+                    return res.json({
+                        success: false,
+                        error: {
+                            field: 'username',
+                            text: 'Username is taken.'
+                        }
+                    });
+                }
+            }
+
+            // other error
+            debug('error save user', err);
+            return res.json({
+                success: false,
+                message: 'Server error, please try again.'
+            });
+        }
+
+        // user successfully saved
+        res.json({
+            success: true,
+            message: 'Account created!'
+        });
+    });
+});
+
+/**
  * Export router.
  */
 module.exports = router;
