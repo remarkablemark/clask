fdd384cf91cf0153ecdfe4b83f470c5e9c569501
Mark <mark@remarkablemark.org>
2016-12-22 15:13:19 -0500

    Have socket.io use the same session middleware
    This ensures that the same session is shared between the http
    instance and the socket.io instance.

--- socket-io.js
+++ socket-io.js
@@ -5,19 +5,26 @@
  */
 const debug = require('debug')(process.env.APP_NAME + ':socket');
 const socket = require('socket.io');
+const session = require('./session');
 const Message = require('./models/message');
 
 /**
- * Socket.IO middleware.
+ * Socket.IO factory.
  *
  * @param {Object} server - The server.
  */
 function io(server) {
     const io = socket(server);
 
+    // use session middleware
+    io.use((socket, next) => {
+        session(socket.request, socket.request.res, next);
+    });
+
     // client connected
     io.on('connection', (socket) => {
-        debug('client connected');
+        const { request } = socket;
+        debug('client connected', request.session);
 
         socket.on('chat:message', (chatMessage) => {
             debug('[chat:message]', chatMessage);
@@ -29,7 +36,7 @@ function io(server) {
         });
 
         socket.on('disconnect', () => {
-            debug('client disconnected');
+            debug('client disconnected', request.session);
         });
     });
 }
