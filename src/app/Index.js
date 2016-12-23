bcad2e3d558998340a6454759c08f13a5aae26c6
Mark <mark@remarkablemark.org>
2016-12-23 18:27:15 -0500

    Refactor app <Index> with <Redirect>

--- src/app/Index.js
+++ src/app/Index.js
@@ -4,43 +4,16 @@
  * Module dependencies.
  */
 import React from 'react';
-import { browserHistory } from 'react-router';
-import { connect } from 'react-redux';
+import Redirect from '../shared/Redirect';
 import Chat from '../chat/Chat';
 
 /**
  * Index component.
  */
-class Index extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-        if (!this.props.isAuthenticated) {
-            browserHistory.push('/signin');
-        }
-    }
-
-    render() {
-        if (!this.props.isAuthenticated) return null;
-
-        return (
+export default function Index() {
+    return (
+        <Redirect unauthenticatedTo='/signin'>
             <Chat />
-        );
-    }
+        </Redirect>
+    );
 }
-
-Index.propTypes = {
-    isAuthenticated: React.PropTypes.bool
-};
-
-function mapStateToProps(state) {
-    return {
-        isAuthenticated: state.user.isAuthenticated
-    };
-}
-
-export default connect(
-    mapStateToProps
-)(Index);
