1afdc61a816c7d1abd3d92a23432fb00b0fb25f9
Mark <mark@remarkablemark.org>
2016-12-22 22:08:09 -0500

    Render session authentication value in template view
    Merge `req.session.isAuthenticated` with `app.locals.public`
    object before rendering view.

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,14 @@
 const router = require('express').Router();
 
 /**
- * GET index page.
+ * GET: *
  */
 router.get('*', (req, res, next) => {
     res.render('index.html', {
-        title: 'Express-Template'
+        title: 'Express-Template',
+        public: Object.assign({}, req.app.locals.public, {
+            isAuthenticated: req.session.isAuthenticated
+        })
     });
 });
 
