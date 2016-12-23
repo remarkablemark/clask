e5befb84f3acbac12c02024819b568c3f7060b98
Mark <mark@remarkablemark.org>
2016-12-23 00:06:03 -0500

    Turn app <Index> into a container component and connect to store
    - Expose prop `isAuthenticated`
      - If the component is not logged in when component mounts,
        redirect to `/signin`

--- src/app/Index.js
+++ src/app/Index.js
@@ -4,13 +4,43 @@
  * Module dependencies.
  */
 import React from 'react';
+import { browserHistory } from 'react-router';
+import { connect } from 'react-redux';
 import Chat from '../chat/Chat';
 
 /**
  * Index component.
  */
-export default function Index() {
-    return (
-        <Chat />
-    );
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        if (!this.props.isAuthenticated) {
+            browserHistory.push('/signin');
+        }
+    }
+
+    render() {
+        if (!this.props.isAuthenticated) return null;
+
+        return (
+            <Chat />
+        );
+    }
 }
+
+Index.propTypes = {
+    isAuthenticated: React.PropTypes.bool
+};
+
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: state.user.isAuthenticated
+    };
+}
+
+export default connect(
+    mapStateToProps
+)(Index);
