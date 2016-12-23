dacf40d9c72a1a236dc768e705dc354daf2974ac
Mark <mark@remarkablemark.org>
2016-12-23 17:00:39 -0500

    Update user reducer with REMOVE_USER action
    This will replace `setAuthentication(false)` as `isAuthenticated`
    will be part of the user object.

--- src/user/actions.js
+++ src/user/actions.js
@@ -4,6 +4,7 @@
  * Action types.
  */
 export const SET_AUTHENTICATION = 'SET_AUTHENTICATION';
+export const REMOVE_USER = 'REMOVE_USER';
 
 /**
  * Action creators.
@@ -12,12 +13,23 @@ export const SET_AUTHENTICATION = 'SET_AUTHENTICATION';
 /**
  * Sets the user authentication value.
  *
- * @param  {Boolean} isAuthentication - The authentication value.
- * @return {Object}                   - The dispatch action.
+ * @param  {Boolean} isAuthenticated - The authentication value.
+ * @return {Object}                  - The dispatch action.
  */
 export function setAuthentication(isAuthenticated) {
     return {
         type: SET_AUTHENTICATION,
         isAuthenticated
+    };
+}
+
+/**
+ * Removes the user.
+ *
+ * @return {Object} - The dispatch action.
+ */
+export function removeUser() {
+    return {
+        type: REMOVE_USER
     };
 }
--- src/user/reducer.js
+++ src/user/reducer.js
@@ -3,7 +3,10 @@
 /**
  * Module dependencies.
  */
-import { SET_AUTHENTICATION } from './actions';
+import {
+    SET_AUTHENTICATION,
+    REMOVE_USER
+} from './actions';
 
 const {
     isAuthenticated
@@ -22,10 +25,17 @@ const initialState = {
  */
 export default function reducer(state = initialState, action) {
     switch (action.type) {
+        // set authentication
         case SET_AUTHENTICATION:
             return Object.assign({}, state, {
                 isAuthenticated: action.isAuthenticated
             });
+
+        // remove user
+        case REMOVE_USER:
+            return {};
+
+        // default
         default:
             return state;
     }
