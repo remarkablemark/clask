88f47d84df7183586fbbbb020179fe62442726e1
Mark <mark@remarkablemark.org>
2016-12-29 17:13:03 -0500

    Send `user` and `users` data from the server-side if authenticated
    This will minimize redundant GET requests if an authenticated
    user happens to refresh the page.
    
    The data will populate the initial state of `user` and `users`
    reducers.

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,43 @@
  * Module dependencies.
  */
 const router = require('express').Router();
+const debug = require('debug')(process.env.APP_NAME + ':db');
+const { Promise } = global;
+const User = require('../models/user');
 
 /**
  * GET: *
  */
 router.get('*', (req, res, next) => {
     const { _id, isAuthenticated } = req.session;
-    res.render('index.html', {
-        title: 'Express-Template',
-        public: Object.assign({}, req.app.locals.public, {
-            user: {
-                _id,
-                isAuthenticated
-            }
-        })
+
+    if (!isAuthenticated) {
+        return res.render('index.html', {
+            title: 'Express-Template'
+        });
+    }
+
+    // authenticated
+    Promise.all([
+        User.findOne({ _id }, { password: 0, __v: 0 }).lean().exec(),
+        User.find({}, { username: 1 }).lean().exec()
+
+    ]).then((results) => {
+        const user = results[0];
+        const users = results[1];
+        user.isAuthenticated = true;
+
+        res.render('index.html', {
+            title: 'Express-Template',
+            public: Object.assign({}, req.app.locals.public, {
+                user,
+                users
+            })
+        });
+
+    }).catch((error) => {
+        debug('unable to find user and users', error);
+        res.status(500).send('Internal Server Error');
     });
 });
 
