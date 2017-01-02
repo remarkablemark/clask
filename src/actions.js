15beadba568ea8567f5f7b5bd3da47e1a8c342f4
Mark <mark@remarkablemark.org>
2017-01-02 18:26:25 -0500

    Export all actions in `src/actions.js`

---
+++ src/actions.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * Export all actions.
+ */
+export * from './shared/actions';
+export * from './messages/actions';
+export * from './user/actions';
+export * from './users/actions';
