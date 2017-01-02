2da3e033ac693a9598d79c95d06a0e4158487b9d
Mark <mark@remarkablemark.org>
2017-01-02 15:39:09 -0500

    Regenerate the session for GET `/signout`
    There is a bug where messages are duplicated when:
    1. Authenticated user visits `/signout`
    2. Since the session was not destroyed, the initial state was still
       rendered as json in `window.__EXPRESS_TEMPLATE__`
    3. Then when the user authenticated, the POST response from
       `/api/auth` contained an identical array of messages
    4. This causes problems with the rendering of <MessageList> and
       React warnings due to duplicate keys
    
    Fix:
    - Regenerate the session for express GET `/signout`
      - Make sure to pass `next()` in the callback so that the
        catch-all route is still hit (html still rendered)
    - GET `/logout` redirects to `/signout`

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,20 @@ const Message = require('../models/message');
 const User = require('../models/user');
 
 /**
+ * GET: /logout
+ */
+router.get('/logout', (req, res, next) => {
+    res.redirect('/signout');
+});
+
+/**
+ * GET: /signout
+ */
+router.get('/signout', (req, res, next) => {
+    req.session.regenerate(() => next());
+});
+
+/**
  * GET: *
  */
 router.get('*', (req, res, next) => {
