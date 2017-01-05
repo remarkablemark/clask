1382a3475a23ca00100c894e6f6e85798b41cc4c
Mark <mark@remarkablemark.org>
2017-01-05 10:47:23 -0500

    Create mongoose model `Room`
    The `Room` model will contain the following fields:
    * _id (generated by shortid)
    * name (defined for channel, undefined for direct message)
    * users (array of user _id's)
    * creator (single user _id)
    * created (date)

---
+++ models/room.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+const mongoose = require('mongoose');
+const generateId = require('shortid').generate;
+
+/**
+ * Room schema.
+ */
+const roomSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        unique: true,
+        default: generateId
+    },
+    // defined for channel
+    // undefined for direct message
+    name: String,
+    users: [
+        {
+            type: String,
+            ref: 'User'
+        }
+    ],
+    creator: {
+        type: String,
+        ref: 'User'
+    },
+    created: Date
+});
+
+/**
+ * Export model for `rooms` collection.
+ */
+module.exports = mongoose.model('Room', roomSchema);
