bb78750f8c6202f98c9e285a36b74689f4e1edd8
Mark <mark@remarkablemark.org>
2017-01-07 00:34:47 -0500

    Add user to default room 'general' when user account created
    Add additional database operation to successful POST `/api/users`

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,10 @@
  */
 const { debug } = require('../../db/helpers');
 const router = require('express').Router();
+const { defaultRoom } = require('../../config/constants');
+
+// models
+const Room = require('../../models/room');
 const User = require('../../models/user');
 
 /**
@@ -65,6 +69,13 @@ router.post('/', (req, res, next) => {
             debug('error save user', err);
             return res.status(500).json({});
         }
+
+        // add user to default room
+        Room.findByIdAndUpdate(defaultRoom, {
+            $push: { _users: user._id }
+        }, (err) => {
+            if (err) debug(`unable to update '${defaultRoom}' room`, err);
+        });
 
         // user successfully saved
         res.json({
