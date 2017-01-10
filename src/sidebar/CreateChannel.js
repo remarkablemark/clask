fff5cfbe3604aeecd16bf81b7b67ef383ab5d8df
Mark <mark@remarkablemark.org>
2017-01-10 17:46:57 -0500

    Update sidebar <CreateChannel> by adding toggle and submit method
    - Add material-ui <Toggle> which toggles `isPublic` value of the
      room (default is false)
    - Currently the submit handler prevents the default event from
      firing

--- src/sidebar/CreateChannel.js
+++ src/sidebar/CreateChannel.js
@@ -4,9 +4,10 @@
  * Module dependencies.
  */
 import React from 'react';
+import _ from 'lodash';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-import _ from 'lodash';
+import Toggle from 'material-ui/Toggle';
 
 // styles
 import { gutter } from '../shared/styles';
@@ -23,35 +24,69 @@ const lastButtonStyle = _.assign({
 export default class CreateChannel extends React.Component {
     constructor() {
         super();
-        this.state = { value: '' };
+        this.state = {
+            isPublic: false,
+            name: ''
+        };
         this._handleChange = this._handleChange.bind(this);
+        this._handleToggle = this._handleToggle.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
     }
 
     /**
      * Listens to input change.
      *
      * @param {Object} event - The event.
-     * @param {String} value - The input value.
+     * @param {String} name  - The input value.
      */
-    _handleChange(event, value) {
-        this.setState({ value });
+    _handleChange(event, name) {
+        this.setState({ name });
+    }
+
+    /**
+     * Listens to input change.
+     *
+     * @param {Object} event    - The event.
+     * @param {String} isPublic - The toggle value.
+     */
+    _handleToggle(event, isPublic) {
+        this.setState({ isPublic });
+    }
+
+    /**
+     * Create channel on form submission.
+     *
+     * @param {Object} event - The event.
+     */
+    _handleSubmit(event) {
+        event.preventDefault();
     }
 
     render() {
         return (
-            <form>
+            <form onSubmit={this._handleSubmit}>
                 <TextField
                     floatingLabelText='Channel Name'
                     fullWidth={true}
-                    value={this.state.value}
+                    value={this.state.name}
                     onChange={this._handleChange}
                     autoFocus
                 />
                 <br />
                 <br />
 
+                <Toggle
+                    label='Public (anyone can join)'
+                    labelPosition='right'
+                    onToggle={this._handleToggle}
+                    toggled={this.state.isPublic}
+                />
+                <br />
+                <br />
+
                 <RaisedButton
                     label='Create Channel'
+                    type='submit'
                     primary={true}
                     style={lastButtonStyle}
                 />
