8c4af1aa828af68a6d74c090b9ce298a6806b163
Mark <mark@remarkablemark.org>
2017-01-09 20:56:58 -0500

    Move all dialog logic from <Sidebar> to <SidebarMenu>

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -4,152 +4,54 @@
  * Module dependencies.
  */
 import React from 'react';
-import _ from 'lodash';
 
 // components
 import Drawer from 'material-ui/Drawer';
 import SidebarMenu from './SidebarMenu';
-import MenuDialog from './MenuDialog';
-import RoomFinder from './RoomFinder';
-import CreateChannel from './CreateChannel';
-import RaisedButton from 'material-ui/RaisedButton';
 
 // constants
-import {
-    dialogPadding,
-    sidebarWidth
-} from '../shared/styles';
+import { sidebarWidth } from '../shared/styles';
 import {
     CHANNELS_TYPE,
-    CREATE_CHANNEL_TYPE,
     DIRECT_MESSAGES_TYPE
 } from './helpers';
 import { defaultRoom } from '../../config/constants';
 
-// styles
-const newChannelButtonStyle = {
-    position: 'absolute',
-    right: dialogPadding
-};
-
 /**
  * Sidebar component.
  */
-export default class Sidebar extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            isDialogOpen: false
-        };
-        this._handleDialogClose = this._handleDialogClose.bind(this);
-    }
+export default function Sidebar(props) {
+    const {
+        activeRoom,
+        rooms,
+        sidebar,
+        users
+    } = props;
 
-    /**
-     * Listens to click from menu or button.
-     *
-     * @param {String} dialogType - The dialog type.
-     */
-    _handleClick(dialogType) {
-        this.setState({
-            dialogType,
-            isDialogOpen: true
-        });
-    }
+    return (
+        <Drawer open={true} width={sidebarWidth}>
+            {/* channels */}
+            <SidebarMenu
+                activeRoom={activeRoom}
+                roomIds={sidebar[CHANNELS_TYPE]}
+                roomPrefix='# '
+                rooms={rooms}
+                title='CHANNELS'
+                type={CHANNELS_TYPE}
+                users={users}
+            />
 
-    _handleDialogClose() {
-        this.setState({
-            isDialogOpen: false
-        });
-    }
-
-    render() {
-        const {
-            activeRoom,
-            rooms,
-            sidebar,
-            users
-        } = this.props;
-
-        const { dialogType, isDialogOpen } = this.state;
-        let dialogNode;
-
-        if (isDialogOpen) {
-            const isChannel = dialogType !== DIRECT_MESSAGES_TYPE;
-            let title = !isChannel && 'DIRECT MESSAGES';
-            if (dialogType === CHANNELS_TYPE) {
-                title = (
-                    <div>
-                        CHANNELS
-                        <RaisedButton
-                            label='NEW CHANNEL'
-                            primary={true}
-                            style={newChannelButtonStyle}
-                            onClick={() => this._handleClick(CREATE_CHANNEL_TYPE)}
-                        />
-                    </div>
-                );
-            } else if (dialogType === CREATE_CHANNEL_TYPE) {
-                title = 'CREATE CHANNEL';
-            }
-
-            const dataSource = isChannel ? _.map(rooms, (value, key) => {
-                return {
-                    text: rooms[key].name,
-                    value: key
-                };
-            }) : _.map(users, (value, key) => {
-                return {
-                    text: users[key].username,
-                    value: key
-                };
-            });
-
-            dialogNode = (
-                <MenuDialog
-                    onRequestClose={this._handleDialogClose}
-                    open={isDialogOpen}
-                    title={title}>
-                    {dialogType === CREATE_CHANNEL_TYPE ? (
-                        <CreateChannel
-                            onRequestClose={this._handleDialogClose}
-                        />
-                    ) : (
-                        <RoomFinder
-                            dataSource={dataSource}
-                            hintText='Find or start a conversation'
-                            onRequestClose={this._handleDialogClose}
-                        />
-                    )}
-                </MenuDialog>
-            );
-        }
-
-        return (
-            <Drawer open={true} width={sidebarWidth}>
-                {/* channels */}
-                <SidebarMenu
-                    activeRoom={activeRoom}
-                    onClick={() => this._handleClick(CHANNELS_TYPE)}
-                    roomIds={sidebar[CHANNELS_TYPE]}
-                    roomPrefix='# '
-                    rooms={rooms}
-                    title='CHANNELS'
-                />
-
-                {/* direct messages */}
-                <SidebarMenu
-                    activeRoom={activeRoom}
-                    onClick={() => this._handleClick(DIRECT_MESSAGES_TYPE)}
-                    roomIds={sidebar[DIRECT_MESSAGES_TYPE]}
-                    rooms={rooms}
-                    title='DIRECT MESSAGES'
-                />
-
-                {/* dialog */}
-                {dialogNode}
-            </Drawer>
-        );
-    }
+            {/* direct messages */}
+            <SidebarMenu
+                activeRoom={activeRoom}
+                roomIds={sidebar[DIRECT_MESSAGES_TYPE]}
+                rooms={rooms}
+                title='DIRECT MESSAGES'
+                type={DIRECT_MESSAGES_TYPE}
+                users={users}
+            />
+        </Drawer>
+    );
 }
 
 Sidebar.propTypes = {
--- src/sidebar/SidebarMenu.js
+++ src/sidebar/SidebarMenu.js
@@ -6,32 +6,50 @@
 import React from 'react';
 import _ from 'lodash';
 
-// material-ui
+// components
 import FontIcon from 'material-ui/FontIcon';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import MenuDialog from './MenuDialog';
+import RoomFinder from './RoomFinder';
+import CreateChannel from './CreateChannel';
+
+// constants
+import { grey300, grey700 } from 'material-ui/styles/colors';
+import {
+    dialogPadding,
+    sidebarMenuItemHeight
+} from '../shared/styles';
+import {
+    CHANNELS_TYPE,
+    CREATE_CHANNEL_TYPE,
+    DIRECT_MESSAGES_TYPE
+} from './helpers';
 
 // styles
-import { grey300, grey700 } from 'material-ui/styles/colors';
-import { sidebarMenuItemHeight } from '../shared/styles';
-
 const menuBaseStyle = {
     minHeight: sidebarMenuItemHeight,
     lineHeight: sidebarMenuItemHeight
 };
 
-const menuHeaderStyle = _.assign({}, menuBaseStyle, {
+const menuHeaderStyle = _.assign({
     color: grey700,
     fontWeight: 'bold'
-});
+}, menuBaseStyle);
 
-const menuItemStyle = _.assign({}, menuBaseStyle, {
+const menuItemStyle = _.assign({
     padding: '0 0.5em'
-});
+}, menuBaseStyle);
 
-const activeMenuStyle = _.assign({}, menuItemStyle, {
+const activeMenuStyle = _.assign({
     backgroundColor: grey300
-});
+}, menuItemStyle);
+
+const newChannelButtonStyle = {
+    position: 'absolute',
+    right: dialogPadding
+};
 
 // menu header icon
 const rightIcon = (
@@ -43,47 +61,138 @@ const rightIcon = (
 /**
  * SidebarMenu component.
  */
-export default function SidebarMenu(props) {
-    const {
-        activeRoom,
-        onClick,
-        roomIds,
-        roomPrefix,
-        rooms,
-        title
-    } = props;
+export default class SidebarMenu extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            dialogType: '',
+            isDialogOpen: false
+        };
+        this._closeDialog = this._closeDialog.bind(this);
+    }
 
-    return (
-        <Menu disableAutoFocus={true}>
-            <MenuItem
-                primaryText={title}
-                rightIcon={rightIcon}
-                style={menuHeaderStyle}
-                onClick={onClick}
-            />
+    /**
+     * Opens dialog when clicked.
+     *
+     * @param {String} dialogType - The dialog type.
+     */
+    _openDialog(dialogType) {
+        this.setState({
+            dialogType,
+            isDialogOpen: true
+        });
+    }
 
-            {_.map(roomIds, (roomId) => {
-                const style = (
-                    roomId === activeRoom ?
-                    activeMenuStyle :
-                    menuItemStyle
+    /**
+     * Closes dialog.
+     */
+    _closeDialog() {
+        this.setState({ isDialogOpen: false });
+    }
+
+    render() {
+        const {
+            activeRoom,
+            roomIds,
+            roomPrefix,
+            rooms,
+            title,
+            type,
+            users
+        } = this.props;
+
+        const { dialogType, isDialogOpen } = this.state;
+        let dialogNode;
+
+        if (isDialogOpen) {
+            const isChannel = dialogType !== DIRECT_MESSAGES_TYPE;
+            let dialogTitle = !isChannel && 'DIRECT MESSAGES';
+            if (dialogType === CHANNELS_TYPE) {
+                dialogTitle = (
+                    <div>
+                        CHANNELS
+                        <RaisedButton
+                            label='NEW CHANNEL'
+                            primary={true}
+                            style={newChannelButtonStyle}
+                            onClick={() => this._openDialog(CREATE_CHANNEL_TYPE)}
+                        />
+                    </div>
                 );
-                return (
-                    <MenuItem style={style} key={roomId}>
-                        {roomPrefix}
-                        {rooms[roomId].name}
-                    </MenuItem>
-                );
-            })}
-        </Menu>
-    );
+            } else if (dialogType === CREATE_CHANNEL_TYPE) {
+                dialogTitle = 'CREATE CHANNEL';
+            }
+
+            const dataSource = isChannel ? _.map(rooms, (value, key) => {
+                return {
+                    text: rooms[key].name,
+                    value: key
+                };
+            }) : _.map(users, (value, key) => {
+                return {
+                    text: users[key].username,
+                    value: key
+                };
+            });
+
+            dialogNode = (
+                <MenuDialog
+                    onRequestClose={this._closeDialog}
+                    open={isDialogOpen}
+                    title={dialogTitle}>
+                    {dialogType === CREATE_CHANNEL_TYPE ? (
+                        <CreateChannel
+                            onRequestClose={this._closeDialog}
+                        />
+                    ) : (
+                        <RoomFinder
+                            dataSource={dataSource}
+                            hintText='Find or start a conversation'
+                            onRequestClose={this._closeDialog}
+                        />
+                    )}
+                </MenuDialog>
+            );
+        }
+
+        return (
+            <Menu disableAutoFocus={true}>
+                {/* title */}
+                <MenuItem
+                    primaryText={title}
+                    rightIcon={rightIcon}
+                    style={menuHeaderStyle}
+                    onClick={() => this._openDialog(type)}
+                />
+
+                {/* rooms */}
+                {_.map(roomIds, (roomId) => {
+                    const style = (
+                        roomId === activeRoom ?
+                        activeMenuStyle :
+                        menuItemStyle
+                    );
+                    return (
+                        <MenuItem style={style} key={roomId}>
+                            {roomPrefix}
+                            {rooms[roomId].name}
+                        </MenuItem>
+                    );
+                })}
+
+                {/* dialog */}
+                {dialogNode}
+            </Menu>
+        );
+    }
 }
 
 SidebarMenu.propTypes = {
     activeRoom: React.PropTypes.string,
-    onClick: React.PropTypes.func,
     roomIds: React.PropTypes.array,
     roomPrefix: React.PropTypes.node,
     rooms: React.PropTypes.object,
-    title: React.PropTypes.string
+    title: React.PropTypes.string,
+    type: React.PropTypes.string,
+    users: React.PropTypes.object
 };
